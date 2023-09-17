5465d111fc8a57385a7c31b61a25e5c5dd19b2c1
Alan <rouille.alan@yahoo.fr>
2023-09-18 00:55:02 +0200

    install jest

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,10 +4,15 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
   return (
     <nav className="nav_header">
-      <Link to="/accueil" class="nav_header_nav-home " tabIndex={0}>
+      <Link
+        to="/accueil"
+        classname="nav_header_nav-home "
+        href="/accueil"
+        tabIndex={0}
+      >
         Accueil
       </Link>
-      <Link to="/about" class="nav_header_nav-propos " tabIndex={0}>
+      <Link to="/about" classname="nav_header_nav-propos " tabIndex={0}>
         A Propos
       </Link>
     </nav>
