f23b2997a70dd6082fba0d41fe245311314b6967
Alan <rouille.alan@yahoo.fr>
2023-09-17 22:22:36 +0200

    modification du css

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
   return (
     <nav className="nav_header">
-      <Link to="/accueil" className="nav_header_nav-home" tabIndex={0}>
+      <Link to="/accueil" class="nav_header_nav-home " tabIndex={0}>
         Accueil
       </Link>
-      <Link to="/about" className="nav_header_nav-propos" tabIndex={0}>
+      <Link to="/about" class="nav_header_nav-propos " tabIndex={0}>
         A Propos
       </Link>
     </nav>
